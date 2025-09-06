21aec293d65b5082ffe0dc32d8f6bd7db3664806
MarcusLipcean <marcuslipcean@gmail.com>
2025-09-06 17:53:55 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-char st[100];
-string s;
+int st[26], a[26], i, n;
 void tipar(int p){
-for(int j=1; j<=p; j++) cout<<st[j];
+for(int j=1; j<=p/2; j++) cout<<st[j]<<" ";
 cout<<endl;
+for(int j=p/2+1; j<=p; j++) cout<<st[j]<<" ";
+cout<<endl<<endl;
+
 }
 bool vrf(int p){
-bool b=true;
-for(int o=1; o<p; o++)
-  if(st[p]==st[o]) b=false;
-return b;
+bool b;
+  b=true;
+  for(int o=1; o<p; o++)
+    if(st[p]==st[o]) b=false;
+if(p!=1 && p!=n+1) return b && st[p]>st[p-1];
+else return b;
 }
 void bktr(int p){
-for(int j=0; j<s.length(); j++){
-st[p]=s[j];
-if(vrf(p)) if(p==s.length()) tipar(p);
+for(int j=1; j<=n*2; j++){
+st[p]=a[j];
+if(vrf(p)) if(p==n*2) tipar(p);
            else bktr(p+1);
 }
 }
 int main()
 {
- cin>>s;
- bktr(1);
+  cin>>n;
+  for(i=1; i<=n*2; i++)
+    cin>>a[i];
+  bktr(1);
     return 0;
 }
