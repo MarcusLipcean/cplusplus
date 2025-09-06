74c6cc483e8a32b135b5fa7913d7964aa93f9248
MarcusLipcean <marcuslipcean@gmail.com>
2025-09-06 17:53:08 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int s, n, a[100];
-char sl[100];
+char st[100];
+string s;
 void tipar(int p){
-for(int j=1;j<=p;j++) cout<<sl[j]<<a[j];
+for(int j=1; j<=p; j++) cout<<st[j];
+cout<<endl;
 }
-int f(int p){
-int sm=0;
-for(int j=1;j<=p;j++)
-  if(sl[j]=='+') sm+=a[j];
-  else sm-=a[j];
-return sm;
+bool vrf(int p){
+bool b=true;
+for(int o=1; o<p; o++)
+  if(st[p]==st[o]) b=false;
+return b;
 }
 void bktr(int p){
-for(int j=1;j<=2;j++){
-if(j==1) sl[p]='+';
-else sl[p]='-';
-if(f(p)<=s) if(f(p)==s && p==n) tipar(p);
-            else bktr(p+1);
+for(int j=0; j<s.length(); j++){
+st[p]=s[j];
+if(vrf(p)) if(p==s.length()) tipar(p);
+           else bktr(p+1);
 }
 }
 int main()
 {
-  cin>>s>>n;
-  for(int i=1;i<=n;i++)
-    cin>>a[i];
-  bktr(1);
+ cin>>s;
+ bktr(1);
     return 0;
 }
